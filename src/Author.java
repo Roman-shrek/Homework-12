5a1e295f80c98c7535ea3761e967cfe8be4f41a6
roman <rom.pyatakov@yandex.ru>
2024-09-17 17:52:16 +0300

    tasks Completed

---
+++ src/Author.java
@@ -0,0 +1,19 @@
+public class Author {
+    private String firsName;
+    private String lastName;
+
+    public Author(String firsName, String lastName) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+
+    }
+
+    public String getFirsName() {
+        return this.firsName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Книга 1");
+        Author mishaBulgakov = new Author("Михаил", "Булгаков");
+        Book masterAndMargarita = new Book("Мастер и Маргарита", mishaBulgakov, 1968);
+        System.out.println("Книга " + masterAndMargarita.getTitleBook());
+        System.out.println("Автор " + mishaBulgakov.getFirsName() + " " + mishaBulgakov.getLastName());
+        masterAndMargarita.setDatePublication(1967);
+        System.out.println("Год публикации " + masterAndMargarita.getDatePublication());
+
+        System.out.println();
+        System.out.println("Книга 2");
+        Author fedyaDostoevky = new Author("Федор", "Достоевский");
+        Book crimeAndPunishment = new Book("Преступление и наказание", fedyaDostoevky, 1890);
+        System.out.println("Книга "+ crimeAndPunishment.getTitleBook());
+        System.out.println("Автор "+ fedyaDostoevky.getFirsName() + " "+ fedyaDostoevky.getLastName());
+        crimeAndPunishment.setDatePublication(1866);
+        System.out.println("Год публикации "+ crimeAndPunishment.getDatePublication());
     }
 }
